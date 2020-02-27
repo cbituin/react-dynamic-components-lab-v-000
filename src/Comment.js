e4cf4467daba5fc7c5274b335e815877d3d054cf
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2020-02-27 18:36:46 +0000

    Automatically backed up by Learn

--- src/Comment.js
+++ src/Comment.js
@@ -1,9 +1,27 @@
 // add Comment component here
 
+<<<<<<< HEAD
 import React, { Component } from "react";
+=======
+import React, { Component } from 'react';
+<<<<<<< HEAD
+import BlogPost from './BlogPost.js';
+>>>>>>> 1782a2301b9250ed529db111ecfe1efed079c15f
 
 export default class Comment extends Component {
   render() {
     return <div className="comment">{this.props.commentText}</div>;
   }
 }
+=======
+
+class Comment extends Component {
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default Comment;
+>>>>>>> 33027ec25ec75677667ec0e71e477753e1dc3720
